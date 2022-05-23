6c8495869eff5eb39d2e8bf82fca03b92487b438
Sahar Apo <sahar@network-leads.com>
2022-05-23 04:56:21 -0400

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,9 @@ function scrollHeader(){
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
 /*=============== SWIPER POPULAR ===============*/
-var swiperPopular = new Swiper(".popular_container", {
+var swiperPopular = new Swiper(".popular__container", {
     spaceBetween: 32,
     grabCursor: true,
     centeredSlides: true,
